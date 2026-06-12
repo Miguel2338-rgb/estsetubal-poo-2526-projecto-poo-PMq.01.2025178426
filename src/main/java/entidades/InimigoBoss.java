bf9efa954e84e6643738827c98197c096e8adb32
Miguel2338-rgb <2025178426@estudantes.ips.pt>
2026-06-12 18:11:07 +0100

    Adicionando as classes InimigoBoss e TorreFranco

---
+++ src/main/java/entidades/InimigoBoss.java
@@ -0,0 +1,14 @@
+package entidades;
+
+import java.awt.*;
+import java.util.List;
+
+public class InimigoBoss extends Inimigo {
+    public InimigoBoss(List<Point> caminho) {
+        super(caminho, 700, 0.9, 90, 5);
+    }
+
+    @Override public Color getCor() { return new Color(120, 0, 180); }
+    @Override public String getNome() { return "Boss"; }
+    @Override public int getRaio() { return 20; }
+}
